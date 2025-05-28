2470b09191376f424ac68b70896cf8e7ca84d1ee
bn-yussuph <sulehajjaajj@gmail.com>
2025-05-28 11:45:30 +0000

    reverted back to one function and made file executable

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,13 +5,10 @@ const request = require('request');
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const xters = JSON.parse(body).characters;
-  extract(xters, 0);
+  for (let i = 0; i < xters.length; i++) {
+    request(xters[i], function (err, res, body) {
+      if (err) throw err;
+      console.log(JSON.parse(body).name);
+    });
+  }
 });
-const extract = (xters, x) => {
-  if (x === xters.length) return;
-  request(xters[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    extract(xters, x + 1);
-  });
-};
