a4b8327f93dcc97929022fc6b74d9aac45025eb5
bn-yussuph <sulehajjaajj@gmail.com>
2025-05-28 11:28:40 +0000

    remove xter extraction to a new function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,10 +5,13 @@ const request = require('request');
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const xters = JSON.parse(body).characters;
-  for (let i = 0; i < xters.length; i++) {
-    request(xters[i], function (err, res, body) {
-      if (err) throw err;
-      console.log(JSON.parse(body).name);
-    });
-  }
+  extract(xters, 0);
 });
+const extract = (xters, x) => {
+  if (x === xters.length) return;
+  request(xters[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    extract(xters, x + 1);
+  });
+};
